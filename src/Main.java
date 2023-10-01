c1c627a9e64872ec02bdf68c58980526e306e6bb
ustim88 <ustandrei88@gmail.com>
2023-10-01 14:58:13 +0300

    ign

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        int initialBalance =100;
+        int addend = 1200;
+        int bonus = 0;
+        if (addend > 1000)
+            bonus = addend / 100;
+    int finalBalance = initialBalance + addend + bonus;
+    System.out.println("итоговый баланс: " + finalBalance);
+    System.out.println("бонус: " + bonus);;
+    }
+}
