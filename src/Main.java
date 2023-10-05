759fff3a42b33133ba664799bc7f3ae34644373c
ustim88 <138520920+ustim88@users.noreply.github.com>
2023-10-05 18:53:33 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main{
         int initialBalance =100;
         int addend = 1200;
         int bonus = 0;
-        if (addend > 1000)
-            bonus = addend / 100;
+        if (addend > 1000) {
+            bonus = addend / 100;}
         int finalBalance = initialBalance + addend + bonus;
         System.out.println("итоговый баланс: " + finalBalance);
         System.out.println("бонус: " + bonus);
